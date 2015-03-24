c14181a57b9c110418ae3a002c4f89cddb23a24b
Muhammed Thanish <mnmtanish@gmail.com>
2015-03-24 10:13:29 +0530

    Support multiple endpoints

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var Client = require('./client');
 
+if(!process.env.ENDPOINTS) {
+  process.exit('Pease set `ENDPOINTS` environment variable');
+}
+
 var MAX_CLIENTS = 1000;
 var NUM_CLIENTS = parseInt(process.env.N) || 1;
-var ENDPOINT = process.env.ENDPOINT || 'http://localhost:8000';
+var ENDPOINTS = process.env.ENDPOINTS.split(',');
 var APP_ID = process.env.APP_ID;
 var APP_SECRET = process.env.APP_SECRET;
 
-for(var i=0; i<NUM_CLIENTS; ++i) {
-  var appId = APP_ID || 'test-app-' + Math.floor(MAX_CLIENTS * Math.random());
-  var appSecret = APP_SECRET || 'test-app-secret';
-  var hostname = 'host-' + Math.floor(Math.random() * 1000000000);
-  var client = new Client(appId, appSecret, hostname);
-  client.start(ENDPOINT);
-}
+ENDPOINTS.forEach(function (endpoint) {
+  for(var i=0; i<NUM_CLIENTS; ++i) {
+    var appId = APP_ID || 'test-app-' + Math.floor(MAX_CLIENTS * Math.random());
+    var appSecret = APP_SECRET || 'test-app-secret';
+    var hostname = 'host-' + Math.floor(Math.random() * 1000000000);
+    var client = new Client(appId, appSecret, hostname);
+    client.start(endpoint);
+  }
+});

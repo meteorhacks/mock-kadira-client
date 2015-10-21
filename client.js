d52224ceb82e62b2c4f716428576ad36f7a34fa8
Muhammed Thanish <mnmtanish@gmail.com>
2015-10-21 18:12:34 +0530

    Measure avg response time

--- client.js
+++ client.js
@@ -69,6 +69,7 @@ Client.prototype.send = function() {
     }
 
     stats.inc(TOTAL_METRICS);
+    stats.dur(Date.now()-time);
   });
 };
 
--- stats.js
+++ stats.js
@@ -1,14 +1,28 @@
-var value = 0;
+var val = 0;
+var res = {
+  val: 0,
+  num: 0,
+};
 
 function inc(n) {
-  value += n;
+  val += n;
+}
+
+function dur(ms) {
+  res.val += ms;
+  res.num++;
 }
 
 setInterval(function () {
-  console.log(value);
-  value = 0;
+  var avg = Math.floor(res.val/res.num);
+  console.log("%d at %d ms res", val, avg);
+
+  val = 0;
+  res.val = 0;
+  res.num = 0;
 }, 1000);
 
 module.exports = {
   inc: inc,
+  dur: dur,
 };

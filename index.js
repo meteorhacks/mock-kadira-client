a7432650de391a344736fb85bc9f9879ebeca6f7
Muhammed Thanish <mnmtanish@gmail.com>
2015-10-22 14:22:41 +0530

    Add node cluster support

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
+var os = require('os');
+var cluster = require('cluster');
 var Client = require('./client');
 
 if(!process.env.ENDPOINTS) {
   process.env.ENDPOINTS = 'http://localhost:11011';
 }
 
+// start node cluster with number of cpus
+if(cluster.isMaster) {
+  for (var i = 0, n = os.cpus().length; i < n; ++i) {
+    cluster.fork();
+  }
+
+  cluster.on('online', function (worker) {
+    console.log('Cluster: worker online', worker.id);
+  });
+
+  cluster.on('exit', function (worker) {
+    console.log('Cluster: worker exit', worker.id);
+    cluster.fork();
+  });
+
+  // do not start the app on cluster master
+  return;
+}
+
 var MAX_CLIENTS = 10000;
-var NUM_CLIENTS = parseInt(process.env.N) || 1;
+var NUM_CLIENTS = parseInt(process.env.N)/os.cpus().length || 1;
 var ENDPOINTS = process.env.ENDPOINTS.split(',');
 var APP_ID = process.env.APP_ID;
 var APP_SECRET = process.env.APP_SECRET;

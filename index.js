208c879f0d10570f79e62506157409ac50aa3489
Muhammed Thanish <mnmtanish@gmail.com>
2015-10-26 17:07:55 +0530

    Log stats only on master

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
 var os = require('os');
 var cluster = require('cluster');
 var Client = require('./client');
+var Stats = require('./stats');
 
-if(!process.env.ENDPOINTS) {
+if (!process.env.ENDPOINTS) {
   process.env.ENDPOINTS = 'http://localhost:11011';
 }
 
-// start node cluster with number of cpus
-if(cluster.isMaster) {
-  for (var i = 0, n = os.cpus().length; i < n; ++i) {
-    cluster.fork();
-  }
-
-  cluster.on('online', function (worker) {
-    console.log('Cluster: worker online', worker.id);
-  });
-
-  cluster.on('exit', function (worker) {
-    console.log('Cluster: worker exit', worker.id);
-    cluster.fork();
-  });
-
-  // do not start the app on cluster master
-  return;
-}
-
 var MAX_CLIENTS = 10000;
-var NUM_CLIENTS = parseInt(process.env.N)/os.cpus().length || 1;
+var NUM_CLIENTS = parseInt(process.env.N) / os.cpus().length || 1;
 var ENDPOINTS = process.env.ENDPOINTS.split(',');
 var APP_ID = process.env.APP_ID;
 var APP_SECRET = process.env.APP_SECRET;
 
-ENDPOINTS.forEach(function (endpoint) {
-  for(var i=0; i<NUM_CLIENTS; ++i) {
-    var appId = APP_ID || 'test-app-' + Math.floor(MAX_CLIENTS * Math.random());
-    var appSecret = APP_SECRET || 'test-app-secret';
-    var hostname = 'host-' + Math.floor(Math.random() * 1000000000);
-    var client = new Client(appId, appSecret, hostname, endpoint);
-    client.start();
+// start node cluster with number of cpus
+if (cluster.isMaster) {
+  spawn();
+} else {
+  start();
+}
+
+function spawn() {
+  for (var i = 0, n = os.cpus().length; i < n; ++i) {
+    var worker = cluster.fork();
+    worker.on('message', function(msg) {
+      Stats.inc(msg.val);
+      Stats.dur(msg.res.val, msg.res.num);
+    });
   }
-});
+
+  cluster.on('online', function(worker) {
+    console.log('Cluster: worker online', worker.id);
+  });
+
+  cluster.on('exit', function(worker) {
+    console.log('Cluster: worker exit', worker.id);
+    cluster.fork();
+  });
+}
+
+function start() {
+  ENDPOINTS.forEach(function(endpoint) {
+    for (var i = 0; i < NUM_CLIENTS; ++i) {
+      var appId = APP_ID || 'test-app-' + Math.floor(MAX_CLIENTS * Math.random());
+      var appSecret = APP_SECRET || 'test-app-secret';
+      var hostname = 'host-' + Math.floor(Math.random() * 1000000000);
+      var client = new Client(appId, appSecret, hostname, endpoint);
+      client.start();
+    }
+  });
+}
--- stats.js
+++ stats.js
@@ -1,3 +1,5 @@
+var cluster = require('cluster');
+
 var val = 0;
 var res = {
   val: 0,
@@ -8,21 +10,42 @@ function inc(n) {
   val += n;
 }
 
-function dur(ms) {
+function dur(ms, n) {
   res.val += ms;
-  res.num++;
+  res.num += (n || 1);
 }
 
-setInterval(function () {
-  var avg = Math.floor(res.val/res.num);
+function log() {
+  var avg = Math.floor(res.val / res.num);
   console.log("%d at %d ms res", val, avg);
+  clr();
+}
 
+function rec() {
+  process.send({
+    val: val,
+    res: res
+  });
+
+  clr();
+}
+
+function clr() {
   val = 0;
   res.val = 0;
   res.num = 0;
-}, 1000);
+}
+
+if (cluster.isMaster) {
+  setInterval(log, 1000);
+} else {
+  setInterval(rec, 100);
+}
 
 module.exports = {
   inc: inc,
   dur: dur,
+
+  val: val,
+  res: res,
 };

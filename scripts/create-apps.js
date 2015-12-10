3a4b5c594aabb20ca05c130da98437281a6b4bca
Muhammed Thanish <mnmtanish@gmail.com>
2015-12-10 14:42:15 +0530

    Tweak create script

--- scripts/create-apps.js
+++ scripts/create-apps.js
@@ -1,6 +1,6 @@
 
 // distribute apps by plans (free plan gets more priority than others)
-var plans = ['free', 'free', 'free', 'solo', 'startup', 'pro', 'business'];
+var plans = ['free', 'solo', 'startup', 'pro', 'business'];
 
 // remove existing temporary apps
 db.apps.remove({__temp__: true});
@@ -10,12 +10,12 @@ for(var i=0; i<10000; ++i) {
   db.apps.insert({
     _id: "test-app-" + i,
     name: "test-app-" + i,
-    created: new Date("2015-02-09T09:08:08.684Z"),
+    created: new Date(),
     owner: "test-app-owner",
     secret: "test-app-secret",
     readonlySecret: "test-app-secret-ro",
     plan: plans[i % plans.length],
-    initialDataReceived: 1424850132466,
+    initialDataReceived: Date.now(),
     __temp__: true,
   });
 }
